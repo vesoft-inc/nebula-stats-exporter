38e99fc496bd0cc856912d09ce94279d8998e31c
knightXun <1004815462@qq.com>
2020-02-10 13:35:15 +0800

    Initial Pr
    add gitigore

---
+++ exporter/go.mod
@@ -0,0 +1,7 @@
+module exporter
+
+require github.com/nebula-stats-exporter/exporter v0.0.1
+
+replace github.com/nebula-stats-exporter/exporter => ../exporter
+
+go 1.13
---
+++ exporter/type.go
@@ -0,0 +1,13 @@
+package exporter
+
+type StaticConfig struct {
+	Version     string       `yaml:"version"`
+	NebulaItems []NebulaItem `yaml:"nebulaItems"`
+}
+
+type NebulaItem struct {
+	InstanceName string `yaml:"instanceName"`
+	EndpointIP   string `yaml:"endpointIP"`
+	EndpointPort int32  `yaml:"endpointPort"`
+	NebulaType   string `yaml:"nebulaType"`
+}
